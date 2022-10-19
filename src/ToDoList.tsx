a09dcc6662007de76339242326855494d7bc197b
gunny6026 <gunny6026@naver.com>
2022-10-19 18:30:56 +0900

    feat : validation 과 error 기능이 있는 useForm
    useForm 을 사용하면 form 에 대한 validation 코드를 작성할 필요가 없다
    또한 에러처리 & 에러메시지를 formState를 사용하여 간단하게 나타낼  수
    있다.

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,49 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  username: string;
+  password: string;
+}
+
 const ToDoList = () => {
-  const { register, watch } = useForm();
+  const { register, handleSubmit, formState } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = () => {};
   return (
     <div>
-      <form>
-        <input {...register("todo")} placeholder='to do 입력' />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "이메일을 입력하세요",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "네이버 메일만 가능합니다.",
+            },
+          })}
+          placeholder='email'
+        />
+        {formState.errors?.email?.message}
+        <input
+          {...register("username", { required: "이름을 입력하세요" })}
+          placeholder='username'
+        />
+        {formState.errors?.username?.message}
+        <input
+          {...register("password", {
+            required: "비밀번호를 입력하세요",
+            minLength: 8,
+          })}
+          placeholder='password'
+        />
+        {formState.errors?.password?.message}
         <button>Add</button>
       </form>
     </div>
